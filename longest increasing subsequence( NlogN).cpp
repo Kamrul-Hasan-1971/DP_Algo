9584ad5c3a3eac9dab3534f021e1370efcf53fc6
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2019-12-24 19:08:46 +0600

    Add files via upload

---
+++ longest increasing subsequence( NlogN).cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define           ll               long long int
+
+vector<ll> v1, v2 ;
+
+ll Fun()
+{
+    v2.push_back( v1[ 0 ] ) ;
+
+    for(ll i = 1; i < v1.size(); i++)
+    {
+        if( v2.back() <= v1[ i ] )
+        {
+            v2.push_back( v1[ i ] ) ;
+        }
+        else
+        {
+            ll pos =  upper_bound( v2.begin(),v2.end(), v1[ i ] )-v2.begin() ;
+            v2[ pos ] = v1[ i ] ;
+        }
+    }
+    return ( ll ) v2.size() ;
+}
+
+int main()
+{
+    ll n, a , i;
+    cin >> n ;
+    for(  i = 0 ; i < n ; i ++ )
+    {
+        cin >> a ;
+        v1.push_back( a ) ;
+    }
+
+    cout << Fun() << endl ;
+    return 0 ;
+}
