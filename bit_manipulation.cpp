07a256c595f300634e7639a21b495ad382d941af
Kamrul_Hasan <48267671+Kamrul-Hasan-1971@users.noreply.github.com>
2021-01-25 09:16:22 +0600

    Update bit_manipulation.cpp

--- bit_manipulation.cpp
+++ bit_manipulation.cpp
@@ -3,8 +3,10 @@ ll sum_of_all_subset ( ll n){
     memset( results, 0, sizeof( results ) ) ;
     for( int i = 0 ; i < ( 1 << n ) ; ++ i ) {
         for ( int j = 0; j < n ; ++ j ) {
-            if ( ( i & ( 1 << j ) ) ! = 0 )
-                results[i] += s [j] ;
+            if ( ( i & ( 1 << j ) ) != 0 )
+            {
+
+            }
         }
     }
 }
